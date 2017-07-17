e4624dff419eeffdfc8c1c759929ebd93b453f92
Hhenry88 <henryhan88@outlook.com>
2017-07-17 14:56:47 -0400

    setup webpack to bundle scss and js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const SRC_DIR = path.resolve(__dirname, 'public');
 const BUILD_DIR = path.resolve(__dirname, 'public/dist');
 
 module.exports = {
-  entry: path.resolve(SRC_DIR, 'src/index.jsx'),
+  entry: [path.resolve(SRC_DIR, 'src/index.jsx'), path.resolve(SRC_DIR,'styles/scss/main.scss')],
   output: {
     filename: 'bundle.js',
     path: BUILD_DIR
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -19,7 +21,23 @@ module.exports = {
         options: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          {fallback: 'style-loader', use: ['css-loader', 'sass-loader']}
+        )
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        sassLoader: {
+          includePaths: ['public/styles/scss']
+        }
+      }
+    }),
+    new ExtractTextPlugin({filename: '[name].bundle.css', allChunks: true})
+  ]
 };
